08ce516baea5c60089c886571d5e366fe6df3001
DESKTOP-QABVANR\apurv <r.apurv2@gmail.com>
2019-05-27 23:00:51 -0700

    commit for branch human

--- src/main/java/com/epam/git-demo/App.java
+++ src/main/java/com/epam/git-demo/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World I am Apurv!" );
+        Human h =new Human("ajay");
+		System.out.println(h.name);
+		
     }
 }
